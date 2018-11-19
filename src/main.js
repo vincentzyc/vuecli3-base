296d92a445bbb3d0bbbece01ee6d6c8147b5bb30
Vincentyun <vincentzyc@qq.com>
2018-11-19 15:50:29 +0800

    首页骨架屏

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import './cube-ui'
 import './plugins/axios'
 import App from './App.vue'
 import router from './router'
-import './assets/css/base.min.css';
+import './assets/css/base.css';
 // import './assets/css/main.css';
 import Api from './assets/js/api';
 import Components from "./modules"; //导入组件库
