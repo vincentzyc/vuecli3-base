efc6034a4e4a4fe62d57e7dee8c6fc106868c7ea
Vincentyun <vincentzyc@qq.com>
2019-01-08 17:44:03 +0800

    规范格式

---
+++ src/views/about.vue
@@ -0,0 +1,10 @@
+<template>
+  <base-page type="about-view" title="About">
+    <div slot="content">
+      <h1>Hello world</h1>
+      <cube-button :primary="true" @click="$router.back()">back</cube-button>
+      <h1>This is an about page</h1>
+      <cube-button :primary="true" @click="$router.push('./button')">button</cube-button>
+    </div>
+  </base-page>
+</template>
---
+++ src/views/home.vue
@@ -0,0 +1,23 @@
+<template>
+  <base-page type="home-view" title="HOME">
+    <div slot="content">
+      <div class="text-center">
+        <img alt="Vue logo" src="../assets/img/logo.png">
+      </div>
+      <HelloWorld/>
+    </div>
+    <base-view></base-view>
+  </base-page>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/hello-world.vue";
+
+export default {
+  name: "home",
+  components: {
+    HelloWorld
+  }
+};
+</script>
