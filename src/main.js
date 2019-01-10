94672870ad0358dc011f1c95d8ab4f93783f7de5
Vincentyun <vincentzyc@qq.com>
2019-01-10 18:27:28 +0800

    结构优化

--- src/main.js
+++ src/main.js
@@ -5,15 +5,14 @@ import App from "./App.vue";
 import router from "./router";
 import "./assets/css/base.css";
 // import './assets/css/main.css';
-import Api from "./assets/js/api";
+import "./assets/js";
 import BaseComponents from "./components/base"; //导入组件库
 
 Vue.config.productionTip = false;
 
-Vue.use(Api);
 Vue.use(BaseComponents);
 
 new Vue({
-  router,
-  render: h => h(App)
+	router,
+	render: h => h(App)
 }).$mount("#app");
